b7cdaf88a4f59ccfb5a7b257038b4286113db874
joewhaley <joewhaley>
2004-09-15 00:34:02 +0000

    Added setmaxincrease, etc.

--- org/sf/javabdd/JDDFactory.java
+++ org/sf/javabdd/JDDFactory.java
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * 
  * @author John Whaley
- * @version $Id: JDDFactory.java,v 1.4 2004/09/14 23:54:10 joewhaley Exp $
+ * @version $Id: JDDFactory.java,v 1.5 2004/09/15 00:34:02 joewhaley Exp $
  */
 public class JDDFactory extends BDDFactory {
 
@@ -21,6 +21,7 @@ public class JDDFactory extends BDDFactory {
     private JDDFactory(int nodenum, int cachesize) {
         bdd = new jdd.bdd.BDD(nodenum, cachesize);
         vars = new int[256];
+        jdd.util.Options.verbose = true;
     }
     
     /* (non-Javadoc)
@@ -547,16 +548,16 @@ public class JDDFactory extends BDDFactory {
      * @see org.sf.javabdd.BDDFactory#setMinFreeNodes(int)
      */
     public void setMinFreeNodes(int x) {
-        // TODO Auto-generated method stub
-        //throw new BDDException();
+        jdd.util.Configuration.minFreeNodesProcent = x;
     }
 
     /* (non-Javadoc)
      * @see org.sf.javabdd.BDDFactory#setMaxIncrease(int)
      */
     public int setMaxIncrease(int x) {
-        //throw new BDDException();
-        return 0;
+        int old = jdd.util.Configuration.maxNodeIncrease;
+        jdd.util.Configuration.maxNodeIncrease = x;
+        return old;
     }
 
     /* (non-Javadoc)
